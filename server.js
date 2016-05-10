dd06bcfea4c2a80f72277dbda4e5200e32bd8c51
greeneat <greeneat2@hotmail.com>
2016-05-10 23:21:54 +0700

    Increate middleware

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
+var middleware = require('./middleware.js'); 
 
-app.get('/',function(req,res){
+/*app.get('/',function(req,res){
 	res.send('Hello world');
+});*/
+
+app.get('/about',middleware.requireAuthentication,function(req,res){
+	res.send('about us');
 });
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000);
